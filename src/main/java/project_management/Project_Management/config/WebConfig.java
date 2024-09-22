ac844db2f75c67c99a57a2a23cbd0f9965e9f3fb
janvi jindal <111556829+janvijindal@users.noreply.github.com>
2024-09-22 08:23:10 +0530

    Update WebConfig.java

--- src/main/java/project_management/Project_Management/config/WebConfig.java
+++ src/main/java/project_management/Project_Management/config/WebConfig.java
@@ -36,7 +36,7 @@ public class WebConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://project-management-orcin-zeta.vercel.app/signing"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://project-management-8indwt6al-janvis-projects-accd4478.vercel.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
